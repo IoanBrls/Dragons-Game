032f1470f3f67726454553de66babc9e0a58fe38
IoanBrls <giannhsmprls@gmail.com>
2019-09-06 22:36:59 +0300

    Adds existing files

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+	public Vector2 StartingVelocity;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		GetComponent<Rigidbody2D>().velocity = StartingVelocity;
+		Destroy(gameObject, 2.2f);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		var enemy = collision.collider.GetComponent<Enemy>();
+
+		enemy?.Die();
+
+		Destroy(gameObject);
+
+	}
+
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+	public GameObject projectilePrefab;
+	
+    void Update()
+    {
+		if (Input.GetButtonDown("Fire2"))
+		{
+			var projectile = GameObject.Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+			//projectilePrefab.GetComponent<AudioSource>().Play();
+		}
+	}
+}
